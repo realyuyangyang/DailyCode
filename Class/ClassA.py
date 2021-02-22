7149d2f55086d759845622b137c046ad2ca45651
YuYangyang <67744789+realyuyangyang@users.noreply.github.com>
2021-02-22 23:16:00 +0800

    Add files via upload

---
+++ Class/ClassA.py
@@ -0,0 +1,21 @@
+class ClassA():
+    var1 = 100
+    var2 = 0.01
+    var3 = 'water'
+
+    def fun1():
+        print('fun1')
+
+    def fun2():
+        print('fun2')
+
+    def fun3():
+        print('fun3')
+
+print(ClassA.var1)
+print(ClassA.var2)
+print(ClassA.var3)
+ClassA.fun1()
+ClassA.fun2()
+ClassA.fun3()
+
